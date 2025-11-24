087b6453d5087a6d8b9a3c49a2c357b0c5500d4c
Mehta Aum <maum5302@gmail.com>
2025-11-25 05:13:55 +0530

    Adding user registration validation and error handling

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,11 +1,32 @@
 import mongoose from "mongoose";
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {apiError} from "../utils/apiError.js"
+import { user } from "../models/user.model.js";
 
 const registerUser = asyncHandler( async (req , res) => {
 
     const {fullName, email, password} = req.body
     
+    if([fullName, email, password].some( (e) => e.trim() = "" ) ){
+        throw new apiError(400, "All fields are required")
+    }
+
+    const users = await user.findOne({email})
     
+    if(users){
+        throw new apiError(400, "user is already exists")
+    }
+
+
+    const userCreated = await user.create({
+        fullName,
+        email,
+        password,
+        roles,
+        avatar: "", 
+        
+
+    })
 
 })
 
