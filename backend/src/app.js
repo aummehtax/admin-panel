ad029121615f9e87b95738cb81683805beb7d083
Mehta Aum <maum5302@gmail.com>
2025-12-18 01:54:22 +0530

    dubugging

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,6 @@ const app = express();
 app.use(cors({
   origin: process.env.CORS_ORIGIN ,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 // ADD THIS FOR DEBUGGING (remove later in production)
